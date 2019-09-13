3ba16ca1104ed018a8a15b65f6cf05158d678398
John Moehrke <JohnMoehrke@gmail.com>
2019-09-13 18:19:12 -0500

    added a set of servers from connectathon. and created an AuditRead

--- AuditWrite.py
+++ AuditWrite.py
@@ -185,20 +185,60 @@ def AuditCheck(client, Query):
 ###app setup
 smart_defaults = {
     #'app_id':'Audit_Test_Bench',
-    'scope':'user/*.* openid profile',
+    #'scope':'user/*.* openid profile',
     'app_id':'4d0dd077-70f4-4896-a2bd-b59aa2f7e02e',    #CE
     'app_secret':'porcupines',
-    'redirect_uri':'http://localhost:8000/fhir-app/',
+    #'redirect_uri':'http://localhost:8000/fhir-app/',
     #'api_base':'http://test.fhir.org/r3',
     #'api_base':'http://test.fhir.org/r4',
     #'api_base':'http://hapi.fhir.org/baseDstu3'
     #'api_base':'http://hapi.fhir.org/baseR4'
-    'api_base':'https://r4.test.pyrohealth.net/fhir'
+    #'api_base':'https://r4.test.pyrohealth.net/fhir'
+	
+	#'api_base': 'http://wildfhir4.aegis.net/fhir4-0-0'
+	# NO -- wildfhir does not seem to auto create audit logs
+	
+	#'api_base': 'http://sqlonfhir-r4.azurewebsites.net/fhir'
+	# ?? -- Telstr Health (HealthConnex) hung on creation of patient
+	
+	#'api_base': 'http://www.pknapp.com:8081/con19'
+	# NO -- pknapp -- failure on patient create --- 400 client error : bsd request
+	
+	#'api_base': 'http://fhir.ext.apelon.com:7080/dts/fhir/'
+	# NO -- apelon is just a termininology server
+	
+	#'api_base': 'http://sandbox.hspconsortium.org/'
+	# NO -- HSPC ... -- somekind of error expectin a value
+	
+	#'api_base': 'http://fhir.tukan.online/'
+	# NO -- Tukan -- failed on patient create
+	
+	#'api_base': 'https://fhir-open.stagingcerner.com/r4/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/'
+	# NO - Cerner -- failure on patient create
+	
+	#'api_base': 'http://r4.heliossoftware.com:8080'
+	# NO -  Helios -- failure on patient create
+	
+	#'api_base': 'http://sandbox.bluebutton.cms.gov'
+	# no - CMS -- FAILURE ON PATIENT CREATE
+	
+	'api_base':'http://elsevier.demo.elsevierfhir.com/fhir'
+	# Elsevier
+
+	
     #'api_base':'https://fhir.careevolution.com/Master.Adapter1.WebClient/api/fhir-r4',
     #'patient_id':'1254099010',
     #'launch_token':'eyJhbGciIDogIlJTMjU2Iiwia2lkIiA6ICJodHRwczovL3Rlc3QuZmhpci5vcmcvcjQvYXV0aC9hdXRoX2tleSIsInR5cCIgOiAiSldUIn0.eyJleHAiIDogMTU2NzA1MDM3MCwiaWF0IiA6ICIxNTY3MDQ2OTI1IiwiaXNzIiA6ICJ0ZXN0LmZoaXIub3JnIiwianRpIiA6ICJ0ZXN0LmZoaXIub3JnL3Nlc3Npb25zLzUzMTA4In0.k4veGpcBqDBLZIU77nsuUi1YLaQGhV59_k9KKEHL0jxRRCJSFOSAMlghuIlQAMOqEwT1O-jy97vkg896P8U1sObnMLxtmhmveWC9uTA0tBv9f9tUt2D7xWFc6QtF2Rh15q1luNnK_Xi4g_HBmZjmfcDV0VquEjaawj661AKjIwMOwRnxx-x_Qd6vPaM-VHXPYMVbo3xB5aRKUs3gKdnwCiOsf7znomnYZXtAA7hDdOd5xQGDu-rGq-6jfdsCVOuZJ21sqhz97ifBvubxNGF7Wsdr7SzJLoglESUCt6SKQhflwT746O_j5c1yJRnS7maFEtDQc0YLfBGqar7om5etSQ'
 }
+print("settings: ")
+print(smart_defaults)
+print("\n")
+
 client = client.FHIRClient(settings=smart_defaults)
+print("client: ")
+print (client)
+print ("\n")
+
 ##client.wants_patient = False
 ##print(client.authorize_url)
 ##client.handle_callback()
